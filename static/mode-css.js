d0d9f359dfbcf9c57b6461ef1b257e17c8dd273a
720x <107641691+720x@users.noreply.github.com>
2022-10-27 12:48:50 +0800

    alex

--- static/mode-css.js
+++ static/mode-css.js
@@ -62,4 +62,8 @@ CodeMirror.defineMode("css", function(config, parserConfig) {
     } else if (ch === "-") {
       if (/[\d.]/.test(stream.peek())) {
         stream.eatWhile(/[\w.%]/);
-   
+        return ret("number", "unit");
+      } else if (stream.match(/^-[\w\\\-]*/)) {
+        stream.eatWhile(/[\w\\\-]/);
+        if (stream.match(/^\s*:/, false))
+          return ret("variable-2", "va
