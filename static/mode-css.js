6e2d72f016e7f734b4e5bdb6b87940a04f64a4c8
720x <107641691+720x@users.noreply.github.com>
2023-01-10 07:33:00 +0800

    some formatting changes

--- static/mode-css.js
+++ static/mode-css.js
@@ -95,4 +95,10 @@ CodeMirror.defineMode("css", function(config, parserConfig) {
       var escaped = false, ch;
       while ((ch = stream.next()) != null) {
         if (ch == quote && !escaped) {
-          if (quote == ")") stream.ba
+          if (quote == ")") stream.backUp(1);
+          break;
+        }
+        escaped = !escaped && ch == "\\";
+      }
+      if (ch == quote || !escaped && quote != ")") state.tokenize = null;
+  
