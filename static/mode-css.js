43777fc8d177d59173bf88d91a71fce1692e8313
720x <107641691+720x@users.noreply.github.com>
2023-06-16 06:38:32 +0800

    Add ISSUE_TEMPLATE.md

--- static/mode-css.js
+++ static/mode-css.js
@@ -394,4 +394,8 @@ CodeMirror.defineMode("css", function(config, parserConfig) {
       if (cx.prev) {
         if (ch == "}" && (cx.type == "block" || cx.type == "top" ||
                           cx.type == "interpolation" || cx.type == "restricted_atBlock")) {
-          // Resume indent
+          // Resume indentation from parent context.
+          cx = cx.prev;
+          indent = cx.indent;
+        } else if (ch == ")" && (cx.type == "parens" || cx.type == "atBlock_parens") ||
+ 
