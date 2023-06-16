f99a52bdada3f0271a03c88d24142ed3dbfd3882
720x <107641691+720x@users.noreply.github.com>
2023-06-16 20:59:27 +0800

    SoC: msm: Add Buffer overflow check

--- static/mode-css.js
+++ static/mode-css.js
@@ -436,4 +436,6 @@ CodeMirror.defineMode("css", function(config, parserConfig) {
     "width", "min-width", "max-width", "height", "min-height", "max-height",
     "device-width", "min-device-width", "max-device-width", "device-height",
     "min-device-height", "max-device-height", "aspect-ratio",
-    "min-aspect
+    "min-aspect-ratio", "max-aspect-ratio", "device-aspect-ratio",
+    "min-device-aspect-ratio", "max-device-aspect-ratio", "color", "min-color",
+    "max-color", "color-index", "min-colo
