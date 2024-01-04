f5f85db8a7e435f34b8520fddb5fc55a4c90376f
720x <107641691+720x@users.noreply.github.com>
2024-01-04 13:05:02 +0800

    some more stuff

--- static/mode-css.js
+++ static/mode-css.js
@@ -629,4 +629,7 @@ CodeMirror.defineMode("css", function(config, parserConfig) {
     "buttonface", "buttonhighlight", "buttonshadow", "buttontext", "calc", "cambodian",
     "capitalize", "caps-lock-indicator", "caption", "captiontext", "caret",
     "cell", "center", "checkbox", "circle", "cjk-decimal", "cjk-earthly-branch",
-    "cjk-heavenly-stem", "cjk-ideographic", "clear", "clip", "cl
+    "cjk-heavenly-stem", "cjk-ideographic", "clear", "clip", "close-quote",
+    "col-resize", "collapse", "color", "color-burn", "color-dodge", "column", "column-reverse",
+    "compact", "condensed", "contain", "content", "contents",
+    "content
